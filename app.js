40612abecc8bb01785979063d939510b139cfd8e
CYFERIOUS <frankdrujes@gmail.com>
2015-06-06 15:51:33 -0500

    60 PERCENT READY

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var jsonObj = require("./data/nav.json");
 var express = require('express');
 var logger = require('morgan');
 var path = require('path');
@@ -8,4 +9,18 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
 
+var way = jsonObj;
+
+
+///server to build the nav
+
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
+  response.write(JSON.stringify(way));
+  response.end();
+});
+server.listen(8080);
 module.exports = app;
+
+
